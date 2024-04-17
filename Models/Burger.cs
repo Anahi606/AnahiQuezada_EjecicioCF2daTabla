b4e42e0af085b141c2c7054f906ee9cb502e6c06
Anahi Quezada <anahi.quezada@udla.edu.ec>
2024-04-17 11:31:55 -0500

    Creacion de la clase Promo

--- Models/Burger.cs
+++ Models/Burger.cs
@@ -4,7 +4,7 @@ namespace AnahiQuezada_EjecicioCF.Models
 {
     public class Burger
     {
-        public int Id { get; set; }
+        public int BurgerId { get; set; }
         [Required]
         public string? Name { get; set; }
         public bool WithCheese { get; set; }
---
+++ Models/Promo.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AnahiQuezada_EjecicioCF.Models
+{
+    public class Promo
+    {
+        public int PromoId { get; set; }
+        [Required]
+        public string? Descripcion { get; set; }
+     
+        public DateTime FechaPromo { get; set; }
+
+        public int BurgerID { get; set; }
+        public Burger? Burger { get; set; }
+    }
+}
