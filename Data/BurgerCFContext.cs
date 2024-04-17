82becc34e8c67290f7dfa29c5e65cdce86567710
laboratorio <laboratorio@UPOAULA10321>
2024-04-17 10:47:04 -0500

    Agregar archivos de proyecto.

---
+++ Data/BurgerCFContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using AnahiQuezada_EjecicioCF.Models;
+
+namespace BurgerCF.Data
+{
+    public class BurgerCFContext : DbContext
+    {
+        public BurgerCFContext (DbContextOptions<BurgerCFContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<AnahiQuezada_EjecicioCF.Models.Burger> Burger { get; set; } = default!;
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AnahiQuezada_EjecicioCF.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
